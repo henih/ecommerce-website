7984dddce90a6c1232a4f1ac33759c778a3a2256
henih <86572062+henih@users.noreply.github.com>
2024-02-07 11:32:36 +0100

    Created checkout page and checkout items and checkout cart functionality

--- ecommerce-website/src/components/product-card/product-card.component.jsx
+++ ecommerce-website/src/components/product-card/product-card.component.jsx
@@ -10,13 +10,13 @@ const ProductCard = ({ product }) => {
   const { addItemToCart } = useContext(CartContext);
 
   const addProductToCart = () => addItemToCart(product);
-  
+
   return (
     <div className="product-card-container">
       <img src={imageUrl} alt={`${name}`} />
       <div className="footer">
         <span className="name">{name}</span>
-        <span className="price">{price}</span>
+        <span className="price">$ {price}</span>
       </div>
       <Button buttonType="inverted" onClick={addProductToCart}>
         Add to cart
